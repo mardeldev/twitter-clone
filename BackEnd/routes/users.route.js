2a53bf3af8585618392bdcdef0a9bfd4a8b5cdd0
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-08-19 17:01:18 +0100

    User login API endpoint completed and working. Added jwt and bcrpyt for password hashing

--- BackEnd/routes/users.route.js
+++ BackEnd/routes/users.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import { userRegisterController } from '../controllers/userRegister.controller.js';
+import { userLoginController } from '../controllers/userLogin.controller.js';
 import { newUserValidation } from '../middleware/chitter.validation.js';
 
 const router = express.Router()
@@ -8,11 +9,11 @@ const router = express.Router()
 
 
 router.route("/register")
-    .post(newUserValidation, userRegisterController)
+    .post(newUserValidation, userRegisterController);
 
 
 router.route("/login")
-    .post();
+    .post(userLoginController);
 
 
 
--- BackEnd/services/user.service.js
+++ BackEnd/services/user.service.js
@@ -1,5 +1,10 @@
 import { UserModel } from '../models/users.model.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+import { config } from "dotenv";
+
+config({ path: `.env${process.env.NODE_ENV ? `.${process.env.NODE_ENV}` : ''}` });
 
 export const userRegisterService = async reqBody => {
     try {
@@ -12,6 +17,26 @@ export const userRegisterService = async reqBody => {
         const newUser = new UserModel({ username: username, password: hashedPassword });
         await newUser.save();
         return { message: "Successfully registered! You can now continue to login." };
+    } catch (error) {
+        throw e;
+    }
+}
+
+export const userLoginService = async reqBody => {
+    try {
+        const { username, password } = reqBody;
+        const user = await UserModel.findOne({ username: username });
+
+        if (!user) { return { message: "Login failed. User does not exist!" } }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) { return { message: "Login failed. Username and password combination is incorrect." } }
+
+        const token = jwt.sign({ id: user._id }, process.env.SECRET);
+
+
+        return { token, userID: user._id };
     } catch (error) {
         throw e;
     }
